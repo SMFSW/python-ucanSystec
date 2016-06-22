64165bf66ebf4144188a1c0be925dcc3e4ab037c
SMFSW <SMFSW@users.noreply.github.com>
2016-06-22 18:48:18 +0200

    python ucanSystec dll bindings: 1st release

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+"""
+__init__.py (ucanSystec)
+Author: SMFSW
+
+init du package SystecUSBCAN
+"""
+
+from ucanSystec import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ucanSystec',
+    version='b0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='SMFSW',
+    author_email='xgoinxpostalx@gmail.com',
+    description='Systec usb/can dll bindings'
+)
