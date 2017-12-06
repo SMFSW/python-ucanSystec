b9b9e9407e8c66aad1124e37d0d0912d2a883d6e
SMFSW <xgoinxpostalx@gmail.com>
2017-12-06 20:03:07 +0100

    v0.2.1: updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 setup(
     name='ucanSystec',
-    version='0.2.0',
-    packages=[''],
+    packages=['ucanSystec'],
+    version='0.2.1',
     url='https://github.com/SMFSW/python-ucanSystec',
     license='MIT',
     author='SMFSW',
     author_email='xgarmanboziax@gmail.com',
-    description='Systec usb/can dll bindings'
+    description='Systec usb/can dll bindings',
+    keywords=['Communication', 'CAN', 'Systec', 'DLL']
 )
